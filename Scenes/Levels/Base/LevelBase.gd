b35bc58af7f7630cfd74c8a22ccc4915847a2668
Amadeusz Jarząb <92082190+AJarzab@users.noreply.github.com>
2024-01-06 19:31:13 +0100

    Docs (#20)
    * added comments to code
    
    * fixed comments

--- Scenes/Levels/Base/LevelBase.gd
+++ Scenes/Levels/Base/LevelBase.gd
@@ -1,16 +1,30 @@
 extends Node2D
 class_name LevelBase
 
+## Variables:
+- **player**: Represents the player object.
+- **collectibles**: Represents a collection of Node2D objects.
+- **effects**: An instance of the "Effects" scene, preloaded and ready to be used.
+- **pauseMenu**: A Control node representing the pause menu.
+- **optionsMenu**: A Control node representing the options menu.
+- **paused**: A boolean indicating whether the game is currently paused.
 @export var player: Player
 @export var collectibles: Node2D
-
 @onready var effects := preload("res://Scenes/Effects/Effects.tscn").instantiate()
 @onready var pauseMenu: Control = $Pause/PauseMenu
 @onready var optionsMenu: Control = $Pause/PauseOptions
 @onready var paused: bool = false
 
-
+## Function: `_ready()`
+- **Description**: Called when the scene is ready. Initializes various components, connects signals, and sets up the initial game state.
+- **Actions**:
+  - Adds the "effects" node as a child with a black background color.
+  - Plays start and game loop sounds.
+  - Connects signals for the pause menu and options menu actions.
 func _ready() -> void:
+	"""
+    Called when the node is added to the scene.
+    """
 	add_child(effects)
 	effects.set_color_background(Color.BLACK)
 	Sounds.play_sound(Sounds.SoundType.START_GAME)
@@ -22,13 +36,30 @@ func _ready() -> void:
 
 	optionsMenu.connect("back", _on_pause_options_back)
 
-
+## Function: `_input(event: InputEvent)`
+- **Description**: Handles input events, specifically the "ui_cancel" action.
+- **Parameters**:
+  - `event`: The InputEvent object containing information about the input event.
+- **Actions**:
+  - Calls the `pause()` function when the "ui_cancel" action is pressed.
 func _input(event: InputEvent) -> void:
+	"""
+    Handles input events.
+    """
 	if event.is_action_pressed("ui_cancel"):
 		pause()
 
+## Function: `pause()`
+- **Description**: Pauses or resumes the game, showing or hiding the pause menu and options menu accordingly.
+- **Actions**:
+  - Toggles the `paused` variable.
+  - Sets the tree paused state.
+  - Shows the pause menu if paused, hides the pause menu and options menu if resumed.
 
 func pause() -> void:
+	"""
+    Pauses or resumes the game.
+    """
 	paused = !get_tree().paused
 	get_tree().paused = paused
 	if paused:
@@ -37,21 +68,48 @@ func pause() -> void:
 		pauseMenu.hide()
 		optionsMenu.hide()
 
-
+## Function: `_on_pause_menu_game_resume()`
+- **Description**: Callback function for the game resume action in the pause menu.
+- **Actions**:
+  - Calls the `pause()` function.
 func _on_pause_menu_game_resume() -> void:
+	"""
+    Resumes the game from the pause menu.
+    """
 	pause()
 
-
+## Function: `_on_pause_menu_quit_to_menu()`
+- **Description**: Callback function for the quit to menu action in the pause menu.
+- **Actions**:
+  - Calls the `pause()` function.
+  - Changes the scene to the main menu.
 func _on_pause_menu_quit_to_menu() -> void:
+	"""
+    Quits to the main menu from the pause menu.
+    """
 	pause()
 	get_tree().change_scene_to_file("res://Scenes/UI/Menu/Main/Menu.tscn")
 
-
+## Function: `_on_pause_menu_options_menu()`
+- **Description**: Callback function for the options menu action in the pause menu.
+- **Actions**:
+  - Hides the pause menu.
+  - Shows the options menu.
 func _on_pause_menu_options_menu() -> void:
+	"""
+    Displays the options menu from the pause menu.
+    """
 	pauseMenu.hide()
 	optionsMenu.show()
 
-
+## Function: `_on_pause_options_back()`
+- **Description**: Callback function for the back action in the options menu.
+- **Actions**:
+  - Shows the pause menu.
+  - Hides the options menu.
 func _on_pause_options_back() -> void:
+	"""
+    Goes back to the pause menu from the options menu.
+    """
 	pauseMenu.show()
 	optionsMenu.hide()
--- Scenes/Levels/TileMap/Spikes/Spikes.gd
+++ Scenes/Levels/TileMap/Spikes/Spikes.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 const SPIKE_DAMAGE = 100
 
-
+## Function: `_on_body_entered(body)`
+- **Description**: Signal handler called when a physics body enters the area of the spike.
+- **Parameters**:
+  - `body`: The physics body that entered the spikes area.
+- **Actions**:
+  - Checks if the entering body is an instance of the `Entity` class.
+  - If true, calls the `take_damage` function on the `Entity` with the specified damage (`SPIKE_DAMAGE`) and the global position of the spike.
 func _on_body_entered(body):
 	if body is Entity:
 		body.take_damage(SPIKE_DAMAGE, global_position)
